75aa4abc8eda6b70992dfccdb3aae149d84de927
eakkawat <eakkawat.prommana.dev@gmail.com>
2020-12-16 22:00:48 +0700

    Create screen

--- snake.py
+++ snake.py
@@ -1,2 +1,20 @@
 import curses
 
+#Set up window
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(1)
+win.nodelay(1)
+
+# Game logic
+
+score = 0
+
+while True:
+    event = win.getch()
+
+curses.endwin()
+print(f"Final scores = {score}")
